38baffee20b684f55c565fc916cba992d13b0a2a
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-03-04 19:28:04 -0300

    Inserir rota POST para salvar compositor no BD.

--- musica-erudita.js
+++ musica-erudita.js
@@ -11,22 +11,46 @@ function getCompositores(response, tipo) {
     response.end(json);
   });
 };
+function salvarCompositor(response, compositor) {
+  CompositoresDB.save(compositor, function(compositor) {
+    console.log("Compositor salvo com sucesso: " + compositor.id);
 
+    let json = JSON.stringify(compositor);
+    response.end(json);
+  });
+};
 
 function callbackServer(request, response) {
   response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
 
   const parts = url.parse(request.url);
 
-  if (parts.path === '/') {
-    response.end("http://localhost:3000/compositores/erudito - http://localhost:3000/compositores/popular");
-  } else if (parts.path === '/compositores/erudito') {
-    getCompositores(response, "erudito");
-  } else if (parts.path === '/compositores/popular') {
-    getCompositores(response, "popular");
-  } else {
-    response.end("Not found: " + parts.path);
+  if (request.method === 'GET') {
+    if (parts.path === '/') {
+      response.end("http://localhost:3000/compositores/erudito - http://localhost:3000/compositores/popular");
+    } else if (parts.path === '/compositores/erudito') {
+      getCompositores(response, "erudito");
+    } else if (parts.path === '/compositores/popular') {
+      getCompositores(response, "popular");
+    } else {
+      response.end("Not found: " + parts.path);
+    }
+  } else if (request.method === 'POST') {
+    let body = '';
+
+    request.on('data', function(data) {
+      body += data;
+    });
+    request.on('end', function() {
+      console.log("POST body: " + body);
+
+      let compositor = JSON.parse(body);
+      salvarCompositor(response, compositor);
+    });
+
+    return;
   }
+
 };
 
 const server = http.createServer(callbackServer);
