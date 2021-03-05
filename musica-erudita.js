e17d0eddabd0fe15ed2bfd82a13438b8b4ec512e
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-03-04 22:44:19 -0300

    Inserir tratamento básico de erro (404 e 500) nas rotas.

--- musica-erudita.js
+++ musica-erudita.js
@@ -15,6 +15,29 @@ app.use(bodyParser.json());
 app.use('/api/compositores', require('./routes/compositores'));
 
 
+// teste de erros
+app.get('/teste-erro', function(req, res) {
+  throw Error('Erro Ninja');
+});
+
+// code 404
+app.use(function(req, res, next) {
+  res.status(404);
+  res.send({ notfound: 'URL não encontrada...'});
+});
+
+// code 500
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+
+  res.status(500);
+  /*
+    Atenção: em Produção é necessário substituir a mensagem de erro do sistema por uma mensagem Genérica!!!
+  */
+  res.json({ msg: 'Ocorreu um erro :: ' + err.message });
+});
+
+
 let server = app.listen(3000, function() {
   let host = server.address().address;
   let port = server.address().port;
