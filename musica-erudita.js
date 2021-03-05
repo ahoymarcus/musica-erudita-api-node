bf2f8d03d21b3dbbf19644ae40460e82dc258dd2
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-03-04 21:45:37 -0300

    Organizar o projeto da API usando uma pasta separada para rotas e chanmando as rotas com o middleware Router. Separar o arquivo de conexão e de CRUD do BD na pasta model.

--- musica-erudita.js
+++ musica-erudita.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const CompositoresDB = require('./CompositoresDB');
 
 
 let app = express();
@@ -10,60 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get('/', function(req, res) {
-  res.send('API dos compositores');
-});
-app.get('/compositores', function(req, res) {
-  CompositoresDB.getCompositores(function(compositores) {
-    res.json(compositores);
-  });
-});
-
 /*
-  Atenção: a rota /compositores/:id deve preceder a /compositores/:tipo
-  A expressão regular (\\d+) é responsável por aceitar apenas números para responsta na rota
+  ROTAS da API Compositores
 */
-app.get('/compositores/:id(\\d+)', function(req, res) {
-  let id = req.params.id;
-
-  CompositoresDB.getCompositoresById(id, function(compositor) {
-    res.json(compositor);
-  });
-});
-app.delete('/compositores/:id(\\d+)', function(req, res) {
-  let id = req.params.id;
-
-  console.log("Deletar compositor id: " + id);
-  CompositoresDB.deleteById(id, function(affectedrows) {
-    res.json({ msg: 'Compositor deletado com sucesso...' });
-  });
-});
-app.get('/compositores/:tipo', function(req, res) {
-  let tipo = req.params.tipo;
-
-  CompositoresDB.getCompositoresByTipo(tipo, function(compositores) {
-    res.json(compositores);
-  });
-});
-
-app.post('/compositores', function(req, res) {
-  let compositor = req.body;
-
-  CompositoresDB.save(compositor, function(compositor) {
-    res.json(compositor);
-  });
-});
-
-app.put('/compositores', function(req, res) {
-  let compositor = req.body;
-
-  CompositoresDB.update(compositor, function(compositor) {
-    res.json(compositor);
-  });
-});
-
-
-
+app.use('/api', require('./routes/compositores'));
 
 
 let server = app.listen(3000, function() {
